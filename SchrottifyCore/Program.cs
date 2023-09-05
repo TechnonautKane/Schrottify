fe1295e88ba879b8b478165377291aee6a138895
user <user@Ubuntu.myguest.virtualbox.org>
2023-09-05 17:59:23 +0100

    ver 0.13

--- SchrottifyCore/Program.cs
+++ SchrottifyCore/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using TagLib;
 
 class Program
 {
@@ -9,15 +10,29 @@ class Program
         LoadMP3Files(musicDirectory);
     }
 
-    static void LoadMP3Files(string directory)
-    {
-        // Get all MP3 files in the directory
-        string[] mp3Files = Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories);
+static void LoadMP3Files(string directory)
+{
+    // Get all MP3 files in the directory
+    string[] mp3Files = Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories);
 
-        // Print out the names of the loaded files
-        foreach (string file in mp3Files)
+    // Process and display details of each MP3 file
+    foreach (string file in mp3Files)
+    {
+        var tfile = TagLib.File.Create(file);
+        Console.WriteLine($"Title: {tfile.Tag.Title ?? "Unknown"}");
+        
+        if (tfile.Tag.Performers.Length > 0)
         {
-            Console.WriteLine(file);
+            Console.WriteLine($"Artist: {tfile.Tag.Performers[0]}");
         }
+        else
+        {
+            Console.WriteLine("Artist: Unknown");
+        }
+        
+        Console.WriteLine($"Album: {tfile.Tag.Album ?? "Unknown"}");
+        Console.WriteLine("------------");
     }
+}
+
 }
