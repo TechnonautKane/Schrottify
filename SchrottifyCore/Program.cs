3bdab3e026b06fbcb2ba80ada3a1d78ee90fd8aa
user <user@Ubuntu.myguest.virtualbox.org>
2023-09-05 17:29:05 +0100

    ver 0.12

---
+++ SchrottifyCore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string musicDirectory = "/home/user/Desktop/Schrottify";
+        LoadMP3Files(musicDirectory);
+    }
+
+    static void LoadMP3Files(string directory)
+    {
+        // Get all MP3 files in the directory
+        string[] mp3Files = Directory.GetFiles(directory, "*.mp3", SearchOption.AllDirectories);
+
+        // Print out the names of the loaded files
+        foreach (string file in mp3Files)
+        {
+            Console.WriteLine(file);
+        }
+    }
+}
